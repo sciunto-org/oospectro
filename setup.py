ac2b850dddd37ed1f9ed38751ed65102186fb97a
François Boulogne <devel@sciunto.org>
2018-03-27 11:09:19 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='oospectro',
+    version='devel',
+    url='https://github.com/sciunto-org/oospectro',
+    maintainer='F. Boulogne',
+    maintainer_email='devel@sciunto.org',
+    license='BSD',
+    platforms=['all'],
+    description='A library that calculates thicknesses from ocean optics spectra.',
+    packages=['oospectro',],
+    provides=['oospectro',],
+    )
