109fb61c9523236ca517ba8943dd5665a830287c
François Boulogne <devel@sciunto.org>
2018-03-27 15:31:45 +0200

    start test IO

--- oospectro/tests/test_io.py
+++ oospectro/tests/test_io.py
@@ -1,5 +1,11 @@
 import pytest
+from numpy.testing import assert_equal
 
 from oospectro import io
 
+path = 'spectra/sample1/000266.xy'
 
+def test_load_file():
+    data = io.load_spectrum(path)
+    assert(len(data) == 2)
+    assert(data[0].shape == data[1].shape)
