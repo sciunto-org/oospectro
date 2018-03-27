998f04f1705f4c15fb7fb36371160aa2dd4492ed
François Boulogne <devel@sciunto.org>
2018-03-27 16:33:29 +0200

    add tests thickness

--- oospectro/tests/test_thickness.py
+++ oospectro/tests/test_thickness.py
@@ -1,4 +1,29 @@
 import pytest
 
-from oospectro import thickness
+import numpy as np
+from numpy.testing import assert_almost_equal
+import os.path
 
+from oospectro import io
+from oospectro.thickness import thickness_from_minmax
+from spectra import known_values
+
+
+
+def test_sample1():
+    min_peak_prominence = 0.02
+    for name, value in known_values.known_thicknesses.items():
+        skipped = ('sample1/011137.xy',
+                   'sample1/012426.xy',
+                   'sample1/012795.xy',
+                   'sample1/012979.xy',
+                   )
+        if name in skipped:
+            continue
+        path = os.path.join('spectra', name)
+        lambdas, intensities = io.load_spectrum(path, lambda_min=450)
+        result = thickness_from_minmax(lambdas, intensities, refractive_index=1.33,
+                                       min_peak_prominence=min_peak_prominence,
+                                       method='ransac', debug=False)
+        error = 100 * np.abs(result - value) / value
+        assert(error < 20)
