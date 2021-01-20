0cbf29aa6a0e20a9d2428045c5c81d9047419e21
François Boulogne <devel@sciunto.org>
2021-01-20 10:40:21 +0100

    Add delimiter to io

--- oospectro/io.py
+++ oospectro/io.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def load_spectrum(spectrum_path, lambda_min=100, lambda_max=1000):
+def load_spectrum(spectrum_path, lambda_min=100, lambda_max=1000,
+                  delimiter=','):
     """
     Load a spectrum file.
 
@@ -13,13 +14,15 @@ def load_spectrum(spectrum_path, lambda_min=100, lambda_max=1000):
         Cut the data at this minimum wavelength in nm.
     lambda_max : scalar, optional
         Cut the data at this maximum wavelength in nm.
+    delimiter : string, optional
+        Delimiter between columns in the datafile.
 
     Returns
     -------
     values : arrays
         (lamdbas, intensities)
     """
-    data = np.loadtxt(spectrum_path, delimiter=',')
+    data = np.loadtxt(spectrum_path, delimiter=delimiter)
     lambdas, intensities = np.column_stack(data)
 
     mask = (lambdas > lambda_min) & (lambdas < lambda_max)
